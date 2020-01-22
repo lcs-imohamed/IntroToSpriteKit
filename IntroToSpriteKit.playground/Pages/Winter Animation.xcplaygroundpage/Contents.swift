8e19f70cf9480634da9535e311dc651c850ffb10
Ilana Mohamed <Imohamed@lcs.on.ca>
2020-01-22 11:19:50 -0500

    Added girl sprite to scene and positioned her. Also imported walking texture images for girl.

--- IntroToSpriteKit.playground/Pages/Winter Animation.xcplaygroundpage/Contents.swift
+++ IntroToSpriteKit.playground/Pages/Winter Animation.xcplaygroundpage/Contents.swift
@@ -12,15 +12,31 @@ let view = SKView(frame: frame)
 view.showsNodeCount = true
 view.presentScene(scene)
 
+//Add winter background to the scene
+let background = SKSpriteNode(imageNamed: "winterAnimationImage")
+background.position = CGPoint(x:scene.size.width / 2, y:scene.size.height / 2)
+scene.addChild(background)
+
+//Increase background size
+let scaleUpAction = SKAction.scale(to: 1.78, duration: 0)
+background.run(scaleUpAction)
 
 
 // Show the view in the live view area
 PlaygroundSupport.PlaygroundPage.current.liveView = view
 
 //Add snow particle effect
-if let snowstorm = SKEmitterNode(fileNamed: "Snow") {
+if let rain = SKEmitterNode(fileNamed: "Snow") {
     snowstorm.position = CGPoint(x: scene.size.width / 2 , y: scene.size.height)
     scene.addChild(snowstorm)
 }
 
+//Add girl sprite to scene and position
+let girl = SKSpriteNode(imageNamed: "2d-sprite-png-10_14")
+girl.position = CGPoint(x: 50, y: 70)
+scene.addChild(girl)
+
+//Create walking textures
+var walkingTextures
+blastOffTextures.append(SKTexture(imageNamed: "rocket_0.png"))
 //: [Next](@next)
