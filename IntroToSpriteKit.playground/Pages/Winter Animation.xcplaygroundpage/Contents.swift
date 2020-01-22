ecb85c93338538d53abf0a273d982a1913b0e0f5
Ilana Mohamed <Imohamed@lcs.on.ca>
2020-01-21 20:10:33 -0500

    Got the snow particle effect to work in my winter animation.

--- IntroToSpriteKit.playground/Pages/Winter Animation.xcplaygroundpage/Contents.swift
+++ IntroToSpriteKit.playground/Pages/Winter Animation.xcplaygroundpage/Contents.swift
@@ -5,18 +5,14 @@ import SpriteKit            // Provides 2D graphics classes
 
 // Define the animation size, scene, and scene color
 let frame = CGRect(x: 0, y: 0, width: 400, height: 300)
-class gameScene: SKScene {
-    var background  = SKSpriteNode(imageNamed: "winterAnimationImage")
-        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
-        addChild(background)
-    
-}
 var scene = SKScene(size: frame.size)
 
 // Define, configure, and present the scene
 let view = SKView(frame: frame)
 view.showsNodeCount = true
 view.presentScene(scene)
+
+
 
 // Show the view in the live view area
 PlaygroundSupport.PlaygroundPage.current.liveView = view
